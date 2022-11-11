cb62af056e587c8943d83721be65bd3a7d2a06d0
S-aoba <writer_fairy_road@yahoo.co.jp>
2022-11-11 18:32:11 +0900

    GitHubPagesに対応させるため

--- client/vite.config.ts
+++ client/vite.config.ts
@@ -3,6 +3,7 @@ import react from '@vitejs/plugin-react';
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: './',
   server: {
     // npm run devした時に自動的にサーバーが立ち上がるようにする
     open: true,
