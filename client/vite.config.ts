4ae4158d1755eb15b3f00ae676b0678eee799742
S-aoba <writer_fairy_road@yahoo.co.jp>
2022-11-11 18:42:45 +0900

    fix

--- client/vite.config.ts
+++ client/vite.config.ts
@@ -3,7 +3,6 @@ import react from '@vitejs/plugin-react';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: './',
   server: {
     // npm run devした時に自動的にサーバーが立ち上がるようにする
     open: true,
