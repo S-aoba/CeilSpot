2e02529d665c9536073f2eaa4793c0fa1498511d
S-aoba <writer_fairy_road@yahoo.co.jp>
2022-11-11 16:48:38 +0900

    fix

--- server/main.py
+++ server/main.py
@@ -1,10 +1,39 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, HTTPException, Request
+from fastapi.middleware.cors import CORSMiddleware
+from fastapi.responses import JSONResponse
+from schemas import SuccessMsg, CsrfSettings
+from routers import route_question, route_answer, route_auth
+from fastapi_csrf_protect import CsrfProtect
+from fastapi_csrf_protect.exceptions import CsrfProtectError
+
 
 app = FastAPI()
+app.include_router(route_question.router)
+app.include_router(route_answer.router)
+app.include_router(route_auth.router)
 
 
-@app.get(
-    "/",
+@CsrfProtect.load_config
+def get_csrf_config():
+    return CsrfSettings()
+
+
+@app.exception_handler(CsrfProtectError)
+def csrf_protect_exception_handler(request: Request, exc: CsrfProtectError):
+    return JSONResponse(status_code=exc.status_code, content={"detail": exc.message})
+
+
+origins = ["http://localhost:3000"]
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
 )
+
+
+@app.get("/", response_model=SuccessMsg)
 def root():
     return {"message": "Success"}
