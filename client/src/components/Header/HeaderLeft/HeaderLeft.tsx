248adffb2e72ae151d8858eac8ad0230103f2b72
S-aoba <writer_fairy_road@yahoo.co.jp>
2022-11-13 22:08:55 +0900

    #25 Fix: UserIcon and Logo

--- client/src/components/Header/HeaderLeft/HeaderLeft.tsx
+++ client/src/components/Header/HeaderLeft/HeaderLeft.tsx
@@ -1,7 +1,7 @@
 import Logo from '../../../assets/logo.png'
 export const HeaderLeft = () => {
   return (
-    <div className=' md:col-span-2 col-span-3 flex items-center justify-center lg:justify-start'>
+    <div className=' col-span-3 flex items-center justify-center lg:col-span-2 lg:justify-start'>
       <img src={Logo} alt='Logo' className=' h-14 w-14' />
     </div>
   )
--- client/src/components/Header/HeaderLight/HeaderLight.tsx
+++ client/src/components/Header/HeaderLight/HeaderLight.tsx
@@ -3,11 +3,15 @@ import { Button } from '../../shared/elements/Button'
 import { Input as SearchBar } from '../../shared/elements/Input'
 import DefaultUserIcon from '../../../assets/defaultUserIcon.png'
 
-export const HeaderLight = () => {
+type Props = {
+  displayWidth: number
+}
+
+export const HeaderLight: React.FC<Props> = ({ displayWidth }) => {
   const [searchValue, setSearchValue] = useState('')
   const onChangeHandler = (e: ChangeEvent<HTMLInputElement>) => setSearchValue(e.target.value)
   return (
-    <div className=' md:col-span-10 col-span-9 flex items-center justify-start gap-3'>
+    <div className='col-span-9 flex items-center justify-around gap-3 lg:col-span-10 lg:justify-start'>
       <SearchBar
         className=' w-8/12 rounded-3xl border py-3 pl-3 outline-sky-400'
         placeholder='Search...'
@@ -17,7 +21,7 @@ export const HeaderLight = () => {
       <Button>
         <img src={DefaultUserIcon} alt='userIcon' className=' h-12 w-12 rounded-full' />
       </Button>
-      <Button className=' btn-info btn text-white hover:opacity-75'>質問する</Button>
+      {displayWidth >= 576 && <Button className=' btn-info btn text-white hover:opacity-75'>質問する</Button>}
     </div>
   )
 }
