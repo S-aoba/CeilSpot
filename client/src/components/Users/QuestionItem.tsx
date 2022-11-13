7572f581b693df2da388b25e4c49c4ff2dd7439a
S-aoba <writer_fairy_road@yahoo.co.jp>
2022-11-13 22:32:38 +0900

    fix add word

--- client/src/components/Users/QuestionItem.tsx
+++ client/src/components/Users/QuestionItem.tsx
@@ -2,7 +2,12 @@ import { Card as QuestionCard } from '../shared/elements/Card'
 
 export const QuestionItem = () => {
   return (
-    <div id='itemList' className=' flex h-fit min-h-screen items-start justify-center py-20 lg:container lg:mx-auto'>
+    <div
+      id='itemList'
+      className=' flex h-fit min-h-screen flex-col items-center justify-start gap-5 py-20 lg:container lg:mx-auto'>
+      <div className=' w-11/12 py-3 text-3xl font-bold xl:w-10/12'>
+        <p>All User Questions</p>
+      </div>
       <div className='grid w-11/12 grid-cols-1 gap-y-5 lg:grid lg:grid-cols-2 lg:gap-x-3 lg:gap-y-10 xl:w-10/12'>
         <QuestionCard />
         <QuestionCard />
