d56fec1266c9732f809872b41570cb92766c05dd
S-aoba <writer_fairy_road@yahoo.co.jp>
2022-11-13 20:39:10 +0900

    fix port

--- client/vite.config.ts
+++ client/vite.config.ts
@@ -1,5 +1,5 @@
-import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react';
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
@@ -7,7 +7,7 @@ export default defineConfig({
     // npm run devした時に自動的にサーバーが立ち上がるようにする
     open: true,
     // デフォルトのポート番号の変更
-    port: 3000,
+    // port: 3000,
   },
   plugins: [react()],
-});
+})
