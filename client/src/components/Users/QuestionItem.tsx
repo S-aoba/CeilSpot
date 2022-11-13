14dc940b5c3b3bd2afa7891593ff311155f24063
S-aoba <writer_fairy_road@yahoo.co.jp>
2022-11-13 21:21:10 +0900

    #23 Fixed: Responsive support for QuestionItem

--- client/src/components/Users/QuestionItem.tsx
+++ client/src/components/Users/QuestionItem.tsx
@@ -2,8 +2,8 @@ import { Card as QuestionCard } from '../shared/elements/Card'
 
 export const QuestionItem = () => {
   return (
-    <div id='itemList' className=' flex h-fit min-h-screen items-start justify-center py-20 md:container md:mx-auto'>
-      <div className='  grid w-full grid-cols-1 gap-y-10 py-3 md:grid md:w-8/12 md:grid-cols-2 md:gap-y-10  md:gap-x-10'>
+    <div id='itemList' className=' flex h-fit min-h-screen items-start justify-center py-20 lg:container lg:mx-auto'>
+      <div className='grid w-11/12 grid-cols-1 gap-y-5 lg:grid lg:grid-cols-2 lg:gap-x-3 lg:gap-y-10 xl:w-10/12'>
         <QuestionCard />
         <QuestionCard />
         <QuestionCard />
--- client/src/components/shared/elements/Card.tsx
+++ client/src/components/shared/elements/Card.tsx
@@ -2,15 +2,16 @@ import LanguageIcon from '../../../assets/LanguageIcon/python.png'
 import DefaultUserIcon from '../../../assets/defaultUserIcon.png'
 
 export const Card: React.FC = () => {
+  const min = ''
   return (
-    <div className=' col-span-1 grid h-32 w-[28rem] grid-cols-12 rounded-xl'>
-      <div className=' col-span-4 flex items-center justify-center'>
-        <img src={LanguageIcon} alt='languageIcon' className=' h-24 w-24 rounded-full' />
+    <div className=' col-span-1 grid h-28 w-full grid-cols-12 rounded-xl '>
+      <div className=' col-span-3 flex w-full items-center justify-center rounded-xl bg-white'>
+        <img src={LanguageIcon} alt='languageIcon' className=' h-16 w-16' />
       </div>
-      <div className=' col-span-8 grid grid-rows-6'>
-        <div className=' row-span-4'>
-          <p className=' text-lg font-semibold line-clamp-3'>
-            新機能をリリースするまでのバックエンド・フロントエンドでの開発手順/PJ進行管理についてのまとめについての考察についてに
+      <div className=' col-span-8 grid grid-rows-6 px-3'>
+        <div className=' row-span-4 flex items-center'>
+          <p className=' text-lg font-semibold line-clamp-2 lg:line-clamp-3'>
+            【Flutter】プログラミング歴半年で、モバイルアプリを2個リリースした話
           </p>
         </div>
         <div className='row-span-2 flex items-center gap-3'>
