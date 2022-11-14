9dbee3a02e16b8871642ed87678d376c8f735369
S-aoba <writer_fairy_road@yahoo.co.jp>
2022-11-14 12:33:29 +0900

    #28 added LinkPath

--- client/src/components/Header/HeaderLeft/HeaderLeft.tsx
+++ client/src/components/Header/HeaderLeft/HeaderLeft.tsx
@@ -1,8 +1,11 @@
+import { Link } from 'react-router-dom'
 import Logo from '../../../assets/logo.png'
 export const HeaderLeft = () => {
   return (
     <div className=' col-span-3 flex items-center justify-center lg:col-span-2 lg:justify-start'>
-      <img src={Logo} alt='Logo' className=' h-14 w-14' />
+      <Link to={'/'}>
+        <img src={Logo} alt='Logo' className=' h-14 w-14' />
+      </Link>
     </div>
   )
 }
