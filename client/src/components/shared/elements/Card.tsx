5525d7f4a86db726e1b1fa9f6ad1ecbdbf4ce4ed
S-aoba <writer_fairy_road@yahoo.co.jp>
2022-11-14 21:38:08 +0900

    #36 Interfacing with API to retrieve all user questions

--- client/src/components/shared/elements/Card.tsx
+++ client/src/components/shared/elements/Card.tsx
@@ -1,12 +1,21 @@
 import LanguageIcon from '../../../assets/LanguageIcon/python.png'
 import DefaultUserIcon from '../../../assets/defaultUserIcon.png'
 import { Link } from 'react-router-dom'
+import { QuestionType } from '../types/types'
 
 type LinkPathProps = {
   path: string
-}
+} & QuestionType
 
-export const Card: React.FC<LinkPathProps> = ({ path }) => {
+export const Card: React.FC<LinkPathProps & QuestionType> = ({
+  path,
+  id,
+  title,
+  body,
+  post_username,
+  answer_list,
+  tags,
+}) => {
   return (
     <div className=' col-span-1 grid h-fit w-full grid-cols-12 rounded-xl'>
       <div className=' col-span-3 flex w-full items-center justify-center rounded-xl bg-white'>
@@ -15,15 +24,13 @@ export const Card: React.FC<LinkPathProps> = ({ path }) => {
       <div className=' col-span-9 grid grid-rows-6 px-3'>
         <div className=' row-span-4 flex items-center'>
           <Link to={path} relative='path' className=' hover:text-sky-400'>
-            <p className=' text-lg font-semibold line-clamp-2 lg:line-clamp-3'>
-              【Flutter】プログラミング歴半年で、モバイルアプリを2個リリースした話
-            </p>
+            <p className=' text-lg font-semibold line-clamp-2 lg:line-clamp-3'>{title}</p>
           </Link>
         </div>
         <div className='row-span-2 flex items-center gap-3'>
           <img src={DefaultUserIcon} alt='userIcon' className=' h-10 w-10 rounded-full' />
           <div className=' w-40'>
-            <p className=' truncate text-sm'>testUser</p>
+            <p className=' truncate text-sm'>{post_username}</p>
           </div>
           <p className=' text-sm text-gray-400'>29日前</p>
         </div>
---
+++ client/src/components/shared/hooks/UseQuery/useQueryQuestions.ts
@@ -0,0 +1,17 @@
+import axios from 'axios'
+import { useQuery } from 'react-query'
+import { QuestionType } from '../../types/types'
+
+export const useQueryQuestions = () => {
+  const getQuestions = async () => {
+    const { data } = await axios.get<QuestionType[]>(`${import.meta.env.VITE_API_URL}/question`, {
+      withCredentials: true,
+    })
+    return data
+  }
+  return useQuery<QuestionType[], Error>({
+    queryKey: 'questions',
+    queryFn: getQuestions,
+    staleTime: Infinity,
+  })
+}
