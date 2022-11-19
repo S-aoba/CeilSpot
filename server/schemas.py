95edfdc33e7b4d0ec65f7a7a4269e3cea13833ac
S-aoba <writer_fairy_road@yahoo.co.jp>
2022-11-19 18:35:59 +0900

    fix

--- server/schemas.py
+++ server/schemas.py
@@ -53,10 +53,10 @@ class AnswerBody(BaseModel):
 class UserInfo(BaseModel):
     id: Optional[str] = None
     username: str
-    self_introduction: str
-    twitter: str
-    github: str
-    website: str
+    self_introduction: Optional[str] = None
+    twitter: Optional[str] = None
+    github: Optional[str] = None
+    website: Optional[str] = None
 
 
 # データベースへ格納する際のデータ型
