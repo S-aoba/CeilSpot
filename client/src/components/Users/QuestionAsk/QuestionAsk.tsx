434cfcfd72cd997bf63a11d9ea11f4f16ad635bc
S-aoba <writer_fairy_road@yahoo.co.jp>
2022-11-22 21:22:58 +0900

    #52 Fix QuestionForm layout

--- client/src/components/Users/QuestionAsk/QuestionAsk.tsx
+++ client/src/components/Users/QuestionAsk/QuestionAsk.tsx
@@ -4,7 +4,7 @@ import { Title } from '../../shared/elements/Title'
 export const QuestionAsk = () => {
   return (
     <div id='post' className=' flex h-fit max-h-screen flex-col items-center pt-3 lg:container lg:mx-auto'>
-      <Title className=' w-11/12 py-3 text-3xl font-bold lg:w-8/12'>Question Form</Title>
+      <Title>Question Form</Title>
       <QuestionForm />
     </div>
   )
