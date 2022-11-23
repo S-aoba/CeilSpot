3aff880039899b200774b308ef7ae1494b138247
S-aoba <writer_fairy_road@yahoo.co.jp>
2022-11-23 22:14:54 +0900

    fix

--- server/routers/route_user.py
+++ server/routers/route_user.py
@@ -23,8 +23,9 @@ async def userInfo_update(request: Request, response: Response, username: str, d
 
 @router.get("/api/user/{username}", response_model=UpdateUser)
 async def get_UserInfo(request: Request, response: Response, username: str, csrf_protect: CsrfProtect = Depends()):
-    new_token = auth.verify_csrf_update_jwt(request, csrf_protect, request.headers)
+    new_token, _ = auth.verify_update_jwt(request)
     res = await db_get_userInfo(username)
+    response.set_cookie(key="access_token", value=f"Bearer {new_token}", httponly=True, samesite="none", secure=True)
     if res:
         return res
     raise HTTPException(status_code=404, detail="userInfo is failed")
