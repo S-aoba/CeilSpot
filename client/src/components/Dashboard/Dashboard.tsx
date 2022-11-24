91f6ccef559a93d7de33e47f542441f1ddd96ac0
S-aoba <writer_fairy_road@yahoo.co.jp>
2022-11-24 16:47:28 +0900

    fix dashboard

--- client/src/components/Dashboard/Dashboard.tsx
+++ client/src/components/Dashboard/Dashboard.tsx
@@ -2,7 +2,6 @@ import { Outlet } from 'react-router-dom'
 import { useQueryUser } from '../../Functional/UseQuery/useQueryUser'
 import { useQueryUserInfo } from '../../Functional/UseQuery/useQueryUserInfo'
 import { Error } from '../Error/Error'
-import { Header } from '../Header/Header'
 import { Loading } from '../Loading/Loading'
 import { Menu } from './DashboardMenu/Menu'
 
@@ -18,21 +17,18 @@ export const Dashboard = () => {
   if (isDataUserNameLoading || isUserInfoLoading) return <Loading />
 
   return (
-    <div className=' min-h-screen bg-slate-200'>
-      <Header />
-      <div className=' flex h-fit min-h-screen flex-col items-center justify-start gap-5 pt-5 lg:container lg:mx-auto'>
-        <Menu username={dataUserName?.username!} />
-        <Outlet
-          context={{
-            id: dataUserInfo?.id,
-            username: dataUserName?.username,
-            self_introduction: dataUserInfo?.self_introduction,
-            twitter: dataUserInfo?.twitter,
-            github: dataUserInfo?.github,
-            website: dataUserInfo?.website,
-          }}
-        />
-      </div>
+    <div className=' flex h-fit min-h-screen flex-col items-center justify-start gap-5 pt-5 lg:container lg:mx-auto'>
+      <Menu username={dataUserName?.username!} />
+      <Outlet
+        context={{
+          id: dataUserInfo?.id,
+          username: dataUserName?.username,
+          self_introduction: dataUserInfo?.self_introduction,
+          twitter: dataUserInfo?.twitter,
+          github: dataUserInfo?.github,
+          website: dataUserInfo?.website,
+        }}
+      />
     </div>
   )
 }
