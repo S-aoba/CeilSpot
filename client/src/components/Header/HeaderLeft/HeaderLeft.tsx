f062af4264ef33751c74bc57bba25e4810f4861e
S-aoba <writer_fairy_road@yahoo.co.jp>
2022-11-29 12:18:26 +0900

    fix Logo Header color

--- client/src/components/Header/HeaderLeft/HeaderLeft.tsx
+++ client/src/components/Header/HeaderLeft/HeaderLeft.tsx
@@ -1,10 +1,11 @@
 import { Link } from 'react-router-dom'
 import Logo from '../../../assets/logo.png'
+
 export const HeaderLeft = () => {
   return (
     <div className=' col-span-3 flex items-center justify-center lg:col-span-2 lg:justify-start'>
-      <Link to={'/'}>
-        <img src={Logo} alt='Logo' className=' h-14 w-14' />
+      <Link to={'/'} className=' flex h-full w-full items-center justify-center'>
+        <img src={Logo} alt='Logo' className='h-16 w-36' />
       </Link>
     </div>
   )
