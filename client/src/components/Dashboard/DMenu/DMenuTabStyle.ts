2a38ccd823cb51f894a62df042636f918481f8b8
S-aoba <writer_fairy_road@yahoo.co.jp>
2022-11-30 22:47:42 +0900

    fix text-color

--- client/src/components/Dashboard/DMenu/DMenuTabStyle.ts
+++ client/src/components/Dashboard/DMenu/DMenuTabStyle.ts
@@ -7,7 +7,7 @@ export const MenuTabStyle = () => {
   const defaultTabStyle =
     'flex h-full w-40 items-center justify-center gap-2 px-3 py-2 text-gray-400 hover:bg-gray-200 rounded-xl hover:text-white'
   const selectedStyle =
-    'flex h-full w-40 items-center justify-center gap-2 px-3 py-2 text-sky-400 rounded-xl bg-sky-400 text-white'
+    'flex h-full w-40 items-center justify-center gap-2 px-3 py-2 rounded-xl bg-sky-400 text-white'
   const questionsStyle = currentMenubarTab === 'question' ? selectedStyle : defaultTabStyle
 
   const answerStyle = currentMenubarTab === 'answer' ? selectedStyle : defaultTabStyle
