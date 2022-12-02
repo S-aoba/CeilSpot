55b92daf983d9274787e781a94378a71f640110e
S-aoba <writer_fairy_road@yahoo.co.jp>
2022-12-02 21:45:56 +0900

    fix

--- client/src/components/QDetail/DetailProfileCard.tsx
+++ client/src/components/QDetail/DetailProfileCard.tsx
@@ -8,13 +8,15 @@ type DetailProfileCardProps = {
 
 export const DetailProfileCard: React.FC<DetailProfileCardProps> = ({ username }) => {
   return (
-    <div className='flex justify-center xl:col-span-3'>
-      <div className='  h-fit max-h-fit w-10/12 rounded-2xl bg-white'>
+    <div className='flex justify-center  xl:col-span-3'>
+      <div className='  h-fit max-h-fit w-11/12 rounded-2xl bg-white'>
         <div className=' flex w-full items-center justify-center py-5'>
           <div className=' flex w-9/12 items-center justify-between'>
-            <AiOutlineUser className=' h-8 w-7 text-gray-400' />
-            <span>質問者: </span>
-            <div className=' flex items-center'>
+            <div className='flex items-center gap-2'>
+              <AiOutlineUser className=' h-5 w-5 text-gray-400' />
+              <span>質問者 : </span>
+            </div>
+            <div className=' flex max-h-fit w-fit items-center'>
               <img src={defaultUserIcon} alt='userIcon' className=' mr-2 h-8 w-8 rounded-full' />
               <p>{username}</p>
             </div>
@@ -25,8 +27,10 @@ export const DetailProfileCard: React.FC<DetailProfileCardProps> = ({ username }
         </div>
         <div className=' flex w-full items-center justify-center py-5'>
           <div className=' flex w-9/12 items-center justify-between'>
-            <SlCalender className=' h-8 w-7 text-gray-400' />
-            <span>公開日: </span>
+            <div className='flex items-center gap-2'>
+              <SlCalender className=' h-5 w-5 text-gray-400' />
+              <span>公開日 : </span>
+            </div>
             <div className=' flex items-center'>
               <p className=' tracking-wide'>2022/12/22</p>
             </div>
