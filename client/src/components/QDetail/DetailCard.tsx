628369ce7190ed5b258377341699d643f422d71a
S-aoba <writer_fairy_road@yahoo.co.jp>
2022-12-02 22:03:07 +0900

    fix tagConponent

--- client/src/components/QDetail/DetailCard.tsx
+++ client/src/components/QDetail/DetailCard.tsx
@@ -15,16 +15,15 @@ export const DetailCard: React.FC<QuestionType & { isDashboard: boolean }> = ({
   return (
     <div className='flex justify-center xl:col-span-9'>
       <div className=' flex h-fit w-6/12 flex-col items-center rounded-xl bg-white py-5 md:w-11/12 xl:w-full'>
-        {isDashboard && (
-          <DetailMenu
-            id={id}
-            title={title}
-            body={body}
-            post_username={post_username}
-            answer_list={answer_list}
-            tags={tags}
-          />
-        )}
+        <DetailMenu
+          id={id}
+          title={title}
+          body={body}
+          post_username={post_username}
+          answer_list={answer_list}
+          tags={tags}
+          isDashboard={isDashboard}
+        />
         <DetailBody body={body} />
       </div>
     </div>
--- client/src/components/QDetail/DetailMenu.tsx
+++ client/src/components/QDetail/DetailMenu.tsx
@@ -4,30 +4,40 @@ import { DetailDeleteBtn } from './DetailDeleteBtn'
 import { DetailEditBtn } from './DetailEditBtn'
 import { DetailTags } from './DetailTags'
 
-export const DetailMenu = ({ id, title, body, post_username, answer_list, tags }: QuestionType) => {
+export const DetailMenu: React.FC<QuestionType & { isDashboard: boolean }> = ({
+  id,
+  title,
+  body,
+  post_username,
+  answer_list,
+  tags,
+  isDashboard,
+}) => {
   return (
     <div className=' flex w-11/12 items-center justify-between'>
       <DetailTags tags={tags} />
-      <div className=' dropdown-end dropdown dropdown-bottom dropdown-hover'>
-        <label tabIndex={0}>
-          <HiOutlineMenu className=' h-10 w-6 hover:cursor-pointer' />
-        </label>
-        <ul tabIndex={0} className='dropdown-content menu rounded-box w-52 bg-base-100 p-2 shadow'>
-          <li>
-            <DetailEditBtn
-              id={id}
-              title={title}
-              body={body}
-              post_username={post_username}
-              answer_list={answer_list}
-              tags={tags}
-            />
-          </li>
-          <li>
-            <DetailDeleteBtn questionId={id} />
-          </li>
-        </ul>
-      </div>
+      {isDashboard && (
+        <div className=' dropdown-end dropdown dropdown-bottom dropdown-hover'>
+          <label tabIndex={0}>
+            <HiOutlineMenu className=' h-10 w-6 hover:cursor-pointer' />
+          </label>
+          <ul tabIndex={0} className='dropdown-content menu rounded-box w-52 bg-base-100 p-2 shadow'>
+            <li>
+              <DetailEditBtn
+                id={id}
+                title={title}
+                body={body}
+                post_username={post_username}
+                answer_list={answer_list}
+                tags={tags}
+              />
+            </li>
+            <li>
+              <DetailDeleteBtn questionId={id} />
+            </li>
+          </ul>
+        </div>
+      )}
     </div>
   )
 }
