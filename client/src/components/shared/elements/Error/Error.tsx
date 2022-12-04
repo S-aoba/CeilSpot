edf40ed3eadbdad1019a3f91f7dbb0abb2b01d29
S-aoba <writer_fairy_road@yahoo.co.jp>
2022-12-04 21:53:09 +0900

    fix Error Loading Screen

--- client/src/components/shared/elements/Error/Error.tsx
+++ client/src/components/shared/elements/Error/Error.tsx
@@ -1,7 +1,11 @@
+import { RootBase } from '../../layout/RootBase'
+
 export const Error = () => {
   return (
-    <div className=' text-center'>
-      <p className='  py-10 font-mono text-6xl'>404 NOT FOUND</p>
-    </div>
+    <RootBase>
+      <div className=' text-center'>
+        <p className='  py-10 font-mono text-6xl'>404 NOT FOUND</p>
+      </div>
+    </RootBase>
   )
 }
--- client/src/components/shared/elements/Loading/Loading.tsx
+++ client/src/components/shared/elements/Loading/Loading.tsx
@@ -1,9 +1,12 @@
 import { BeatLoader } from 'react-spinners'
+import { RootBase } from '../../layout/RootBase'
 
 export const Loading = () => {
   return (
-    <div className=' text-center'>
-      <BeatLoader color=' #38bdf8' size={20} className='py-10' />
-    </div>
+    <RootBase>
+      <div className=' text-center'>
+        <BeatLoader color=' #38bdf8' size={20} className='py-10' />
+      </div>
+    </RootBase>
   )
 }
