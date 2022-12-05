172e961888d2f597e195881919290842aac98813
S-aoba <writer_fairy_road@yahoo.co.jp>
2022-12-05 17:21:56 +0900

    fix the footer at the bottom

--- client/src/components/Footer/Footer.tsx
+++ client/src/components/Footer/Footer.tsx
@@ -5,7 +5,7 @@ import { Links } from './Links'
 
 export const Footer = () => {
   return (
-    <div className=' flex justify-center bg-sky-100 pt-16 pb-5 tracking-wider'>
+    <div className=' flex justify-center bg-sky-100 pt-16 pb-5 tracking-wider flex-1'>
       <div className=' flex w-11/12 justify-center gap-x-16 pb-20'>
         <div className=' flex flex-col items-center gap-y-3 px-5 pt-5'>
           <img src={Logo} alt='logo' className=' h-10' />
