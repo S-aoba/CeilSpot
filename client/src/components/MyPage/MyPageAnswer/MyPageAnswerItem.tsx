8ef41e4be80c222ef004da274d30816c0d0e1146
S-aoba <writer_fairy_road@yahoo.co.jp>
2022-12-08 16:47:05 +0900

    fix QuestionItem height MyPageAnswer Link navigate

--- client/src/components/MyPage/MyPageAnswer/MyPageAnswerItem.tsx
+++ client/src/components/MyPage/MyPageAnswer/MyPageAnswerItem.tsx
@@ -1,12 +1,14 @@
 import DefaultUserIcon from '../../../assets/defaultUserIcon.png'
-import { Link } from 'react-router-dom'
+import { Link, useNavigate } from 'react-router-dom'
 import { useQuerySingleQuestion } from '../../../functional/UseQuery/useQuerySingleQuestion'
 import { AnswerType } from '../../../types/types'
 import { QItemLanguageIcon } from '../../QList/QItemLanguageIcon'
 import { Error } from '../../shared/elements/Error/Error'
 import { Loading } from '../../shared/elements/Loading/Loading'
+import { title } from '@uiw/react-md-editor'
 
 export const MyPageAnswerItem: React.FC<AnswerType> = ({ id, body, question_id, respondent_username }) => {
+  const navigate = useNavigate()
   const { data: dataQuestion, isLoading: isDataQuestionLoading, error } = useQuerySingleQuestion(question_id)
   if (error) return <Error />
   if (isDataQuestionLoading) return <Loading />
@@ -14,19 +16,21 @@ export const MyPageAnswerItem: React.FC<AnswerType> = ({ id, body, question_id, 
   return (
     <>
       {dataQuestion && (
-        <div className=' flex h-24 w-[26rem] rounded-3xl border-[3px] bg-white duration-500 hover:relative hover:translate-x-2 hover:-translate-y-2 hover:shadow-[-8px_8px_0px_0px_#171717] lg:w-[29rem]'>
+        <div
+          onClick={() => {
+            navigate(`/${dataQuestion.post_username}/question/${dataQuestion.id}`, {
+              state: {
+                id: dataQuestion.id,
+                isDashboard: true,
+              },
+            })
+          }}
+          className=' col-span-1 flex h-[6.6rem] w-[26rem] rounded-3xl border-[3px] bg-white duration-500 hover:relative hover:translate-x-2 hover:-translate-y-2 hover:cursor-pointer hover:shadow-[-8px_8px_0px_0px_#171717] lg:w-[29rem]'
+        >
           <QItemLanguageIcon firstTag={dataQuestion.tags[0]} />
           <div className=' grid w-full grid-rows-6 py-2 px-2'>
             <div className=' row-span-4 flex items-center'>
-              <Link
-                to={`/${dataQuestion.post_username}/question/${dataQuestion.id}`}
-                state={{
-                  id: dataQuestion.id,
-                  isDashboard: true,
-                }}
-              >
-                <p className=' text-lg font-semibold line-clamp-2 hover:text-sky-400'>{body}</p>
-              </Link>
+              <p className=' text-lg font-semibold line-clamp-2 hover:underline hover:underline-offset-4'>{body}</p>
             </div>
             <div className=' row-span-2 flex items-center justify-start gap-4'>
               <img src={DefaultUserIcon} alt='userIcon' className=' h-8 w-8 rounded-full' />
