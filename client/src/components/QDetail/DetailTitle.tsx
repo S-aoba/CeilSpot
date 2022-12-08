679f243d2e2d9d30088ebb7bf6e4aa3a1ff027ae
S-aoba <writer_fairy_road@yahoo.co.jp>
2022-12-08 14:21:18 +0900

    fix change QuestionDetai Title width

--- client/src/components/QDetail/DetailTitle.tsx
+++ client/src/components/QDetail/DetailTitle.tsx
@@ -6,7 +6,7 @@ type DetailTitleProps = {
 
 export const DetailTitle: React.FC<DetailTitleProps> = ({ children }) => {
   return (
-    <div className=' w-10/12 py-5 font-mono text-2xl font-bold tracking-wide lg:w-fit lg:max-w-fit'>
+    <div className=' mt-5 w-10/12 py-5 font-mono text-2xl font-bold tracking-wide lg:w-9/12'>
       <p>{children}</p>
     </div>
   )
