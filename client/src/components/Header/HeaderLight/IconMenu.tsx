9839cb344eb07afbe81dbdf6a675e5e8d34f9297
S-aoba <writer_fairy_road@yahoo.co.jp>
2022-12-08 12:21:39 +0900

    fix rm HeaderRight menuIcon myPageLink

--- client/src/components/Header/HeaderLight/IconMenu.tsx
+++ client/src/components/Header/HeaderLight/IconMenu.tsx
@@ -1,21 +1,13 @@
-import { BsQuestionSquare } from 'react-icons/bs'
 import { BiLogOut } from 'react-icons/bi'
-import { CgProfile } from 'react-icons/cg'
-import { AiOutlineComment } from 'react-icons/ai'
-import { Link } from 'react-router-dom'
 import DefaultUserIcon from '../../../assets/defaultUserIcon.png'
-import { useAppDispatch, useAppSelector } from '../../../app/hooks'
 import { useQueryUserIdAndUsername } from '../../../functional/UseQuery/useQueryUserIdAndUsername'
 import { Loading } from '../../shared/elements/Loading/Loading'
 import { QuestionList } from '../../../pages/QList'
 import { useProcessAuth } from '../../../functional/hooks/UserProcess/useProcessAuth'
-import { changeMenubarTab, selectMenubarTab } from '../../../slices/menuBarSlice'
 
 export const IconMenu: React.FC = () => {
   const { logout } = useProcessAuth()
-  const dispatch = useAppDispatch()
   const { data: userIdAndUsername, isLoading, error } = useQueryUserIdAndUsername()
-  const currentMenuBarTabType = useAppSelector(selectMenubarTab)
 
   if (error) return <QuestionList />
   if (isLoading) return <Loading />
@@ -33,48 +25,6 @@ export const IconMenu: React.FC = () => {
             tabIndex={0}
             className='dropdown-content menu rounded-box w-52 gap-3 bg-base-100 p-2 text-gray-400 shadow'
           >
-            <li>
-              <Link
-                to={`/dashboard/question`}
-                relative='path'
-                className='text-base hover:bg-gray-100 hover:text-black focus:bg-white active:bg-gray-100'
-                state={userIdAndUsername.userId}
-                onClick={() => dispatch(changeMenubarTab({ ...currentMenuBarTabType, myPageMenu: 'myQuestion' }))}
-              >
-                <span>
-                  <BsQuestionSquare />
-                </span>
-                <p>投稿した質問</p>
-              </Link>
-            </li>
-            <li>
-              <Link
-                to={`/dashboard/answer`}
-                relative='path'
-                className=' text-base hover:bg-gray-100 hover:text-black focus:bg-white active:bg-gray-100'
-                state={userIdAndUsername.userId}
-                onClick={() => dispatch(changeMenubarTab({ ...currentMenuBarTabType, myPageMenu: 'myAnswer' }))}
-              >
-                <span>
-                  <AiOutlineComment />
-                </span>
-                <p>回答した質問</p>
-              </Link>
-            </li>
-            <li>
-              <Link
-                to={`/dashboard/profile`}
-                relative='path'
-                className=' text-base hover:bg-gray-100 hover:text-black focus:bg-white active:bg-gray-100'
-                state={userIdAndUsername.userId}
-                onClick={() => dispatch(changeMenubarTab({ ...currentMenuBarTabType, myPageMenu: 'myProfile' }))}
-              >
-                <span>
-                  <CgProfile />
-                </span>
-                <p>プロフィール</p>
-              </Link>
-            </li>
             <li>
               <div
                 className='flex text-base hover:bg-gray-100 hover:text-black focus:bg-white active:bg-gray-100'
