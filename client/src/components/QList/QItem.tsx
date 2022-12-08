b3be8d2a379d372c968da815e6ba2c10bb5eefd7
S-aoba <writer_fairy_road@yahoo.co.jp>
2022-12-08 15:01:08 +0900

    fix QuestionItem navigate

--- client/src/components/QList/QItem.tsx
+++ client/src/components/QList/QItem.tsx
@@ -1,3 +1,4 @@
+import { useNavigate } from 'react-router-dom'
 import { QuestionType } from '../../types/types'
 import { QItemLanguageIcon } from './QItemLanguageIcon'
 import { QItemTitle } from './QItemTitle'
@@ -18,20 +19,28 @@ export const QuestionItem: React.FC<LinkPathProps & QuestionType> = ({
   tags,
   isDashboard,
 }) => {
+  const navigate = useNavigate()
+
   return (
-    <div className=' flex h-24 w-[26rem] rounded-3xl border-[3px] bg-white duration-500 hover:relative hover:translate-x-2 hover:-translate-y-2 hover:shadow-[-8px_8px_0px_0px_#171717] lg:w-[29rem]'>
+    <div
+      onClick={() => {
+        navigate(`${path}`, {
+          state: {
+            id,
+            title,
+            body,
+            post_username,
+            answer_list,
+            tags,
+            isDashboard,
+          },
+        })
+      }}
+      className=' flex h-24 w-[26rem] rounded-3xl border-[3px] bg-white duration-500 hover:relative hover:translate-x-2 hover:-translate-y-2 hover:cursor-pointer hover:shadow-[-8px_8px_0px_0px_#171717] lg:w-[29rem]'
+    >
       <QItemLanguageIcon firstTag={tags[0]} />
       <div className=' grid w-full grid-rows-6 py-2 px-2'>
-        <QItemTitle
-          path={path}
-          id={id}
-          title={title}
-          body={body}
-          post_username={post_username}
-          answer_list={answer_list}
-          tags={tags}
-          isDashboard={isDashboard}
-        />
+        <QItemTitle title={title} />
         <QItemUserInfo post_username={post_username} answer_list={answer_list} />
       </div>
     </div>
--- client/src/components/QList/QItemTitle.tsx
+++ client/src/components/QList/QItemTitle.tsx
@@ -1,31 +1,11 @@
-import { Link } from 'react-router-dom'
-import { QuestionType } from '../../types/types'
-
 type QItemTitleProps = {
-  path: string
-  isDashboard: boolean
-} & QuestionType
+  title: string
+}
 
-export const QItemTitle: React.FC<QItemTitleProps> = ({
-  path,
-  id,
-  title,
-  body,
-  post_username,
-  answer_list,
-  tags,
-  isDashboard,
-}) => {
+export const QItemTitle: React.FC<QItemTitleProps> = ({ title }) => {
   return (
     <div className=' row-span-4 flex items-center'>
-      <Link
-        to={path}
-        relative='path'
-        className=' hover:text-sky-400'
-        state={{ id, title, body, post_username, answer_list, tags, isDashboard }}
-      >
-        <p className=' text-lg font-semibold line-clamp-2'>{title}</p>
-      </Link>
+      <p className=' text-lg font-semibold line-clamp-2 hover:underline hover:underline-offset-4'>{title}</p>
     </div>
   )
 }
