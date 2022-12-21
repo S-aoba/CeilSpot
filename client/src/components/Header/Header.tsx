696a98f4afd47b539cb7a87b1bca7fa0b3dcbf9c
S-aoba <writer_fairy_road@yahoo.co.jp>
2022-12-21 11:19:28 +0900

    Fix: change Header bg

--- client/src/components/Header/Header.tsx
+++ client/src/components/Header/Header.tsx
@@ -6,7 +6,7 @@ type HeaderProps = {
 }
 export const Header: React.FC<HeaderProps> = ({ isAuth }) => {
   return (
-    <div className=' fixed top-0 z-10 flex h-14 max-h-14 w-full items-center justify-center bg-white border-b'>
+    <div className=' fixed top-0 z-10 flex h-14 max-h-14 w-full items-center justify-center bg-blue-100 border-b'>
       <div className=' container mx-auto grid grid-cols-12 pl-5'>
         <HeaderLeft />
         <HeaderLight isAuth={isAuth} />
