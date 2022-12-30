0b6a095149d11cf7e9fb4b0d3dd579099c5523b0
S-aoba <writer_fairy_road@yahoo.co.jp>
2022-12-30 15:22:59 +0900

    Fix: rm HeaderRight searchBar input in shared element

--- client/src/components/Header/HeaderLight/HeaderLight.tsx
+++ client/src/components/Header/HeaderLight/HeaderLight.tsx
@@ -1,9 +1,6 @@
-import { BiSearchAlt2 } from 'react-icons/bi'
 import { useLayoutEffect } from 'react'
-import { Input as SearchBar } from '../../shared/elements/Input'
 import { Login } from './Login'
 import { SignUp } from './SignUp'
-import { useHeaderLight } from './useHeaderLight'
 import { IconMenu } from './IconMenu'
 import { useScreen } from '../../hooks/useScreen'
 import { useDispatch } from 'react-redux'
@@ -17,7 +14,6 @@ type Props = {
 export const HeaderLight: React.FC<Props> = ({ isAuth }) => {
   const dispatch = useDispatch()
   const currentMenuBarTabType = useAppSelector(selectMenubarTab)
-  const { onChangeSearchValue, searchValue } = useHeaderLight()
   const { screenWidth, screenWidthMonitoring } = useScreen()
 
   useLayoutEffect(() => {
@@ -26,16 +22,6 @@ export const HeaderLight: React.FC<Props> = ({ isAuth }) => {
 
   return (
     <div className=' col-span-7 flex h-10 max-h-10 items-center justify-center gap-3 px-2 lg:col-span-10 lg:justify-end'>
-      {screenWidth > 992 ? (
-        <SearchBar
-          className=' h-5/6 w-5/12 rounded-lg border pl-3 outline-sky-400'
-          placeholder='Search...'
-          value={searchValue}
-          onChange={onChangeSearchValue}
-        />
-      ) : (
-        <BiSearchAlt2 className=' h-9 w-7 text-gray-400 hover:cursor-pointer' />
-      )}
       {isAuth ? (
         <>
           <IconMenu />
