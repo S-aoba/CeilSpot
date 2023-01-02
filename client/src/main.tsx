f45a9d823dff0b3a03ece6f419cdc76f5188a31e
S-aoba <writer_fairy_road@yahoo.co.jp>
2023-01-02 18:50:56 +0900

    Fix: reactQurty queryClient

--- client/src/main.tsx
+++ client/src/main.tsx
@@ -7,14 +7,7 @@ import './index.css'
 import { ToastContainer } from 'react-toastify'
 import 'react-toastify/dist/ReactToastify.css'
 import { ReactQueryDevtools } from 'react-query/devtools'
-const queryClient = new QueryClient({
-  defaultOptions: {
-    queries: {
-      retry: false,
-      refetchOnWindowFocus: false,
-    },
-  },
-})
+import { queryClient } from './lib/reactQuery'
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
   <QueryClientProvider client={queryClient}>
